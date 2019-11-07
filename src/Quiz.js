ea50528c6ef85de7a9fdaa77e6b9e521ae4e5d54
Peter Hoppe <peterhoppe341@gmail.com>
2019-11-07 09:56:35 -0600

    Added feature of tracking correct answers
    Added the ability to track the number of correct answers that were answered.

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,12 +8,18 @@ class Quiz extends Component {
 
   constructor(props){
     super(props);
-    this.state = {quiz_position: 1}
+    this.state = {quiz_position: 1,
+                  quiz_correct: 0}
   }
 
-  showNextQuestion(){
+  showNextQuestion(guessedIncorrectly){
     this.setState((state) => {
-      return {quiz_position: state.quiz_position + 1}
+      if (guessedIncorrectly)
+        return {quiz_position: state.quiz_position + 1}
+      else {
+        return {quiz_position: state.quiz_position + 1,
+                quiz_correct: state.quiz_correct + 1}
+      }
     })
   }
 
@@ -25,8 +31,13 @@ class Quiz extends Component {
     const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
     return (
       <div>
+        <div className= "QuizInfo">
+          {isQuizEnd ? <p className = "QuestionMarker">Quiz Finished!</p>  : <p className = "QuestionMarker"> Question: {this.state.quiz_position}</p>}
+          <p className = "ScoreDisplay">Number Correct: {this.state.quiz_correct}</p>
+        </div>
         {isQuizEnd ? <QuizEnd resetClickHandler = {this.handleResetClick.bind(this)}/> :
-        <QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)} quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+        <QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)} quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>
+      }
       </div>
     )
   }
